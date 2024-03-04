38d4754d8392b4cd423b21356036e93440fa1fdf
Rodrigo Azevedo <rodrigoazevedo@rodrigos-mbp.speedport.ip>
2024-03-04 13:04:32 +0100

    removed old migration files

--- network/admin.py
+++ network/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post, Comment, Like, UserProfile
+from .models import Post, Comment, Like, UserProfile, User
 
 # Register your models here.
+admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Like)
